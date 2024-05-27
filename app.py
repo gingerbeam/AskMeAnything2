f9b074c86c7debd8fe4336bf60bc47609158ba6b
beam-magnum <ayhu.pku@outlook.com>
2024-05-28 02:25:36 +0800

    fix model bugs

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ from intel_extension_for_transformers.neural_chat import build_chatbot
 from intel_extension_for_transformers.neural_chat import plugins
 # from intel_extension_for_transformers.transformers import RtnConfig
 
+#模型下载
+from modelscope import snapshot_download
+embedding_model_dir = snapshot_download('AI-ModelScope/bge-base-zh-v1.5')
+llm_dir = snapshot_download('ZhipuAI/chatglm3-6b')
+
 plugins.retrieval.enable=True
-plugins.retrieval.args['embedding_model'] = "ZhipuAI/chatglm3-6b"
+plugins.retrieval.args['embedding_model'] = embedding_model_dir
+# plugins.retrieval.args['embedding_model'] = "AI-ModelScope/bge-base-zh-v1.5"
 plugins.retrieval.args['process'] = False
 plugins.retrieval.args["input_path"]="./mental_health.txt"
 
@@ -14,7 +20,8 @@ plugins.retrieval.args["input_path"]="./mental_health.txt"
 # plugins=plugins,
 # optimization_config=RtnConfig(compute_dtype="int8",
 # weight_dtype="int4_fullrange"))
-config = PipelineConfig(model_name_or_path='AI-ModelScope/bge-base-zh-v1.5', plugins=plugins)
+config = PipelineConfig(model_name_or_path=llm_dir, plugins=plugins)
+# config = PipelineConfig(model_name_or_path='ZhipuAI/chatglm3-6b', plugins=plugins)
 print("Config finish!")
 
 chatbot = build_chatbot(config)
